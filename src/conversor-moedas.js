6647c0ec248a593aaf119d3e786ae0577d560bb8
Daniella Dutra <danivetdutra@gmail.com>
2022-01-04 09:25:27 -0300

    Criado campo de texto

--- src/conversor-moedas.js
+++ src/conversor-moedas.js
@@ -1,13 +1,27 @@
 import './conversor-moedas.css';
-import { Button, Form, Col, Spinner, Container } from 'react-bootstrap';
+import { Form, Col, Row, Container, Spinner, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
 
 function ConversorMoedas() {
-  return;
-  <div>
-    <h1>Conversor de Moedas</h1>;<Container></Container>
-  </div>;
+  return (
+    <div>
+      <h1>Conversor de Moedas</h1>
+      <Container>
+        <Form>
+          <Row>
+            <Col sm="3">
+              <Form.Control placeholder="0" value={1} required />
+            </Col>
+            <Col sm="3"></Col>
+            <Col sm="1"></Col>
+            <Col sm="3"></Col>
+            <Col sm="2"></Col>
+          </Row>
+        </Form>
+      </Container>
+    </div>
+  );
 }
 
 export default ConversorMoedas;
