2cf3252c9a54d2ab53ca73a841655d634871de60
Daniella Dutra <danivetdutra@gmail.com>
2022-01-04 09:03:15 -0300

    Realizado import da font do icone seta-dupla e bootstrap

--- src/conversor-moedas.js
+++ src/conversor-moedas.js
@@ -1,7 +1,13 @@
 import './conversor-moedas.css';
+import { Button, Form, Col, Spinner, Container } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
 
 function ConversorMoedas() {
-  return <h1>Conversor de Moedas</h1>;
+  return;
+  <div>
+    <h1>Conversor de Moedas</h1>;<Container></Container>
+  </div>;
 }
 
 export default ConversorMoedas;
