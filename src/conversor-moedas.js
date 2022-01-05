0c98c99ad633e734a55785cc4d5bae8562693d8d
Daniella Dutra <danivetdutra@gmail.com>
2022-01-05 13:23:24 -0300

    Adicionado alert  em caso de erro de conversao dos dados

--- src/conversor-moedas.js
+++ src/conversor-moedas.js
@@ -1,5 +1,5 @@
 import './conversor-moedas.css';
-import { Form, Col, Button, Row, Spinner } from 'react-bootstrap';
+import { Form, Col, Button, Row, Spinner, Alert } from 'react-bootstrap';
 import './index.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
@@ -8,6 +8,9 @@ function ConversorMoedas() {
   return (
     <div>
       <h1>Conversor de Moedas</h1>
+      <Alert variant="danger" show={true}>
+        Erro ao obter dados de conversão, tente novamente!
+      </Alert>
       <Form>
         <Row>
           <Col sm="3">
