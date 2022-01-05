f99b640f2dbd0a3ab18bc03aa8072af4d79f1f71
Daniella Dutra <danivetdutra@gmail.com>
2022-01-05 15:01:49 -0300

    Adicionado modal para aplicacao

--- src/conversor-moedas.js
+++ src/conversor-moedas.js
@@ -1,5 +1,14 @@
 import './conversor-moedas.css';
-import { Form, Col, Button, Row, Spinner, Alert } from 'react-bootstrap';
+import {
+  Form,
+  Col,
+  Button,
+  Row,
+  Spinner,
+  Alert,
+  Modal,
+  Container,
+} from 'react-bootstrap';
 import './index.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
@@ -8,31 +17,43 @@ function ConversorMoedas() {
   return (
     <div>
       <h1>Conversor de Moedas</h1>
-      <Alert variant="danger" show={true}>
-        Erro ao obter dados de conversão, tente novamente!
-      </Alert>
-      <Form>
-        <Row>
-          <Col sm="3">
-            <Form.Control placeholder="0" value={1} required />
-          </Col>
-          <Col sm="3">
-            <Form.Control as="select"></Form.Control>
-          </Col>
-          <Col sm="1" className="text-center" style={{ paddingTop: '5px' }}>
-            <FontAwesomeIcon icon={faAngleDoubleRight}></FontAwesomeIcon>
-          </Col>
-          <Col sm="3">
-            <Form.Control as="select"></Form.Control>
-          </Col>
-          <Col sm="2">
-            <Button variant="success" type="submit">
-              <Spinner animation="border" size="sm" />
-              Converter
-            </Button>
-          </Col>
-        </Row>
-      </Form>
+      <Container>
+        <Alert variant="danger" show={true}>
+          Erro ao obter dados de conversão, tente novamente!
+        </Alert>
+        <Form>
+          <Row>
+            <Col sm="3">
+              <Form.Control placeholder="0" value={1} required />
+            </Col>
+            <Col sm="3">
+              <Form.Control as="select"></Form.Control>
+            </Col>
+            <Col sm="1" className="text-center" style={{ paddingTop: '5px' }}>
+              <FontAwesomeIcon icon={faAngleDoubleRight}></FontAwesomeIcon>
+            </Col>
+            <Col sm="3">
+              <Form.Control as="select"></Form.Control>
+            </Col>
+            <Col sm="2">
+              <Button variant="success" type="submit">
+                <Spinner animation="border" size="sm" />
+                Converter
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+        <Modal show={true}>
+          <Modal.Header type="button" class="close" aria-label="Close">
+            <Modal.Title>Conversão</Modal.Title>
+            <span aria-hidden="true">X</span>
+          </Modal.Header>
+          <Modal.Body>Resultado da conversão aqui...</Modal.Body>
+          <Modal.Footer>
+            <Button variant="success">Nova conversão</Button>
+          </Modal.Footer>
+        </Modal>
+      </Container>
     </div>
   );
 }
